8bbd2ffecdc159a6b4b90c98bdbccab39cae846a
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2023-05-29 16:56:51 -0700

    added login view

--- Main/seeds/seed.js
+++ Main/seeds/seed.js
@@ -1,23 +1,33 @@
+
+// Checked
+
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Blog, Comment } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
+  await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
+  console.log(User);
+
+  for (const blog of blogData){
+    await Blog.create({
+      ...blog,
+      // userId: users[Math.floor(Math.random()*users.length)].id,
+    })
   }
+
+  const comments = await Comment.bulkCreate(commentData, {
+    returning:true,
+});
 
   process.exit(0);
 };
